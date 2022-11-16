fe70a1d2710434316f9177079a4ba15745f5da7b
Anatolij007 <98270281+Anatolij007@users.noreply.github.com>
2022-11-16 02:30:58 +0200

    complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import { useState } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
